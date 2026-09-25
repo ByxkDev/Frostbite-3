540e704f081b9e5c504c9f41ecf72a59a897258b
Byxk <voormijnps4@gmail.com>
2026-09-25 20:16:37 +0200

    Refactor main.go for improved readability and security

--- Frostbite3/Battlefield4/main.go
+++ Frostbite3/Battlefield4/main.go
@@ -20,10 +20,10 @@ import (
 const (
 	RedirectorHostname  = "gosredirector.ea.com"
 	RedirectorPort      = 42127
-	BlazeServerHostname = "151.244.72.66"
+	BlazeServerHostname = "151.xxx.xxx.xx"
 	BlazeServerPort     = 33152
 	CertificatePath     = "network/certificates/gosredirector_mod.pfx"
-	CertificatePassword = "123456"
+	CertificatePassword = "password"
 )
 
 func loadPFX(path, password string) (tls.Certificate, error) {
